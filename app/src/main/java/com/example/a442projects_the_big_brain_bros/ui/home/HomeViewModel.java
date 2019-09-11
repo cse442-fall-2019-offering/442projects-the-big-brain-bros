c2eb5cf883094ffc3efb0e0c690a5cd5cd28728c
Jonathan Wong <JonathanWong198@gmail.com>
2019-09-11 16:21:52 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/a442projects_the_big_brain_bros/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.a442projects_the_big_brain_bros.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
