a5d68303ac85ecd7c960b289dc6db7b24efd014a
Jonathan Wong <JonathanWong198@gmail.com>
2019-09-18 14:09:21 -0400

    Created a mock-recipe page made using CardView that will appear after searching. Added new font folder containing roboto font. Added mock-recipe images in drawable folder

---
+++ app/src/main/java/com/example/a442projects_the_big_brain_bros/RecipeList.java
@@ -0,0 +1,15 @@
+package com.example.a442projects_the_big_brain_bros;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_list);
+    }
+
+}
+
