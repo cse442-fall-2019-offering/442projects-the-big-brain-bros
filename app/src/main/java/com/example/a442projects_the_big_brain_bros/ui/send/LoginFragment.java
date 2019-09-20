733f6b5643ee70779a7b92ac0c692ffe705dd464
Tamnacious <tam.carmen711@gmail.com>
2019-09-20 02:29:24 -0400

    Created a sign up and login page for the user which is accessible through the navigation menu. Changed text display for the navigation bar (Shows appropriate tabs) Also added additional drawables into the folder.

--- app/src/main/java/com/example/a442projects_the_big_brain_bros/ui/send/LoginFragment.java
+++ app/src/main/java/com/example/a442projects_the_big_brain_bros/ui/send/LoginFragment.java
@@ -5,14 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.a442projects_the_big_brain_bros.R;
 
-public class SendFragment extends Fragment {
-
+public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/com/example/a442projects_the_big_brain_bros/ui/send/RegisterFragment.java
+++ app/src/main/java/com/example/a442projects_the_big_brain_bros/ui/send/RegisterFragment.java
@@ -5,17 +5,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.a442projects_the_big_brain_bros.R;
 
-public class SendFragment extends Fragment {
+public class RegisterFragment extends Fragment {
 
     @Nullable
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_login,container,false);
+        return inflater.inflate(R.layout.activity_register,container,false);
     }
 }
