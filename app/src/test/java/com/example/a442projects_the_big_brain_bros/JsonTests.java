303e54ca67ff485e1cabbf9e9b8da58eb14ae2f1
Jordan Fluker <jtfluker@buffalo.edu>
2019-10-14 12:40:04 -0400

    Branch: Recipe-List-UI Added the outline for some JSON tests that will be useful for testing the JSON API once it is complete

---
+++ app/src/test/java/com/example/a442projects_the_big_brain_bros/JsonTests.java
@@ -0,0 +1,19 @@
+package com.example.a442projects_the_big_brain_bros;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class JsonTests {
+
+    // Test that the json objects we are obtaining through the API are valid
+    @Test
+    public void Test_json_valid(){
+
+    }
+
+    //Test the contents of a returned json object to make sure they match the desired results
+    @Test
+    public void Test_json_values(){
+
+    }
+}
