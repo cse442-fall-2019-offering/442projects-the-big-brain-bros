69601a12ead42659a795cdb31f5b9e647a40cbe7
JonathanWong198 <JonathanWong198@gmail.com>
2019-11-10 01:54:14 -0500

    Task #43 : Added a Save Button to the Recipe page

--- app/src/main/java/com/example/a442projects_the_big_brain_bros/RecipeActivity.java
+++ app/src/main/java/com/example/a442projects_the_big_brain_bros/RecipeActivity.java
@@ -3,6 +3,8 @@ package com.example.a442projects_the_big_brain_bros;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -34,6 +36,26 @@ public class RecipeActivity extends AppCompatActivity {
         setContentView(textView);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.recipe_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.save_button) {
+            // do something here
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
