5ca5acf5b0a40631c539fae20d67e3f3d73cfa13
Tamnacious <tam.carmen711@gmail.com>
2019-11-18 12:10:10 -0500

    #57 Updated History Recipe View to match Recipe View from Search

--- app/src/main/java/com/example/a442projects_the_big_brain_bros/HistoryRecipeActivity.java
+++ app/src/main/java/com/example/a442projects_the_big_brain_bros/HistoryRecipeActivity.java
@@ -4,14 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class HistoryRecipeActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+public class HistoryRecipeActivity extends AppCompatActivity {
+    boolean mIsSaved = true;
     //    public TextView textView = (TextView) findViewById(R.id.textV);
     @Override
 
@@ -20,24 +30,159 @@ public class HistoryRecipeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_recipe);
         TextView textView = new TextView(this);
-        textView.setTextSize(20);
-        String text = HistoryActivity.title +"\n\n"  + "Ingredients: \n";
-
-        for(int i = 0; i < HistoryActivity.ingredientList.size(); i++){
-            text += HistoryActivity.ingredientList.get(i) + "\n";
+        TextView title = (TextView) findViewById(R.id.textT);
+        title.setText(RecipeList.title + "\n");
+        String text = "";
+        TextView ingredients = (TextView) findViewById(R.id.textIL);
+        for(int i = 0; i < RecipeList.ingredientList.size(); i++){
+            text += RecipeList.ingredientList.get(i) + "\n";
         }
-        text += "\n";
-        for(int i = 0; i < HistoryActivity.instruction.size(); i++){
-            text += "Step " + (i+1) + ")\n" +  HistoryActivity.instruction.get(i) + "\n\n";
+        ingredients.setText(text + "\n");
+        TextView instruction = (TextView) findViewById(R.id.textSL);
+        text = "";
+        for(int i = 0; i < RecipeList.instruction.size(); i++){
+            text += "STEP " + (i+1) + ". \n\n" +  RecipeList.instruction.get(i) + "\n\n";
         }
-        textView.setText(text);
-        textView.setMovementMethod(new ScrollingMovementMethod());
-        setContentView(textView);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
-//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        instruction.setText(text);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // R.menu.mymenu is a reference to an xml file named mymenu.xml which should be inside your res/menu directory.
+        // If you don't have res/menu, just create a directory named "menu" inside res
+        getMenuInflater().inflate(R.menu.recipe_action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean result = true;
+        switch (item.getItemId()) {
+            case R.id.save_button:
+                if (mIsSaved) { //you could modify this to check the icon/text of the menu item
+
+                    String title;
+                    FileOutputStream fos = null;
+                    try{
+                        fos = openFileOutput("FAVORITE_RECIPES.txt", MODE_APPEND);
+                        title = RecipeList.title;
+                        fos.write(title.getBytes());  //write the text to the local storage
+
+                        fos = null;
+
+                        Toast.makeText(getApplicationContext(), "Saved to" + getFilesDir() + "/" + "FAVORITE_RECIPES", Toast.LENGTH_LONG).show();
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }finally {
+                        if (fos != null){
+                            try {
+                                fos.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    mIsSaved = false;
+                } else {
+
+                    ArrayList<String> title = new ArrayList<>();
+                    FileInputStream fis = null;
+                    try {
+                        fis = openFileInput("FAVORITE_RECIPES.txt");
+                        InputStreamReader isr = new InputStreamReader(fis);
+                        BufferedReader br = new BufferedReader(isr);
+                        StringBuilder sb = new StringBuilder();
+                        String text;
+                        while ((text = br.readLine()) != null){
+                            sb.append(text).append("\n");
+                            if(!(text.equals(RecipeList.title)))
+                                title.add(text);
+                        }
+
+                        isr = null;
+                        br = null;
+                        sb = null;
+                        fis = null;
+
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }finally {
+                        if (fis != null){
+                            try {
+                                fis.close();
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+
+                    }
+
+
+
+                    FileOutputStream fos = null;
+                    try{
+
+
+                        fos = openFileOutput("FAVORITE_RECIPES.txt", MODE_APPEND);
+                        String joinedTitle = String.join("\n", title);
+                        fos.write(joinedTitle.getBytes());  //write the text to the local storage
+                        fos = null;
+                        Toast.makeText(getApplicationContext(), "Unsaved to" + getFilesDir() + "/" + "FAVORITE_RECIPES", Toast.LENGTH_LONG).show();
+                    }catch (FileNotFoundException e){
+                        e.printStackTrace();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }finally {
+                        if (fos != null){
+                            try {
+                                fos.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+
+                    mIsSaved = true;
+
+                }
+                invalidateOptionsMenu(); //cause a redraw
+                break;
+            default:
+                result = super.onOptionsItemSelected(item);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (mIsSaved) {
+            //in production you'd probably be better off keeping a reference to the item
+            menu.findItem(R.id.save_button)
+                    .setIcon(R.drawable.white_border_heart)
+                    .setTitle("Save");
+        } else {
+            menu.findItem(R.id.save_button)
+
+                    .setIcon(R.drawable.white_heart)
+                    .setTitle("Unsave");
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
 }
+
+
 
